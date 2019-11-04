2802dbead5d7654e4a0c49f2289ff5a3099c28eb
Park Jongho <kim010c@gmail.com>
2019-11-04 22:27:50 +0900

    온도 값 호출

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import Weather from "./Weather";
 
-export default function App() {
-  return <Loading />;
+import axios from "axios";
+
+const API_KEY = "babf349c8e13f65004e951907e4e5d3f";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.", "미안");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+  }
 }
